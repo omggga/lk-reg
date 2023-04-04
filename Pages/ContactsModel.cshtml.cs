efc8e62b690c722b79986f2c4a8d3b23deeb77ef
omggga <omggga@gmail.com>
2023-04-04 11:24:44 +0700

    Experimental commit 3

---
+++ Pages/ContactsModel.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lk_reg.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
